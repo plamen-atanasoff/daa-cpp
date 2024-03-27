bad1fb2cae7e6fa88a784190e7b4464be10107e7
Plamen Atanasov <plamen.atanasov25@gmail.com>
2024-03-27 13:38:06 +0200

    Implement counter for monument distance bigger than r

---
+++ Solutions/MonumentDistance.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+const int SIZE = 300'000;
+long long n, r, i, j, cnt;
+long long arr[SIZE];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	// entries are sorted in ascending order
+	cin >> n >> r;
+	for (i = 0; i < n; i++) cin >> arr[i];
+
+	for (i = 0; i < n - 1; i++) {
+		while (j < n && arr[j] - arr[i] <= r) j++;
+		cnt += n - j;
+	}
+
+	cout << cnt;
+}
