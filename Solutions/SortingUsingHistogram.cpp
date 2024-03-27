168a986e85f74765c76b8aaf28316caa6378fd3c
Plamen Atanasov <plamen.atanasov25@gmail.com>
2024-03-27 12:25:22 +0200

    Implement sorting with histogram

---
+++ Solutions/SortingUsingHistogram.cpp
@@ -0,0 +1,26 @@
+// useful if there are a lot of values and the values are in a smaller range
+#include <iostream>
+using namespace std;
+
+const int SIZE = 1000;
+int n, num;
+int arr[SIZE];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> num;
+		arr[num]++;
+	}
+
+	for (int i = 0; i < SIZE; i++) {
+		while (arr[i] > 0) {
+			cout << i << " ";
+			arr[i]--;
+		}
+	}
+}
