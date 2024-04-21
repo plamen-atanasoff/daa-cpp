a2f16c52b78e8f6c3eca666bbb257d09b1392631
Plamen Atanasov <plamen.atanasov25@gmail.com>
2024-04-21 17:11:57 +0300

    Solve find cube root problem

---
+++ Solutions/CubeRoot.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+double x, l, r, mid;
+
+void cubeRoot() {
+	cin >> x;
+
+	r = max(double(1), x);
+	for (int i = 0; i < 100; i++) {
+		mid = l + (r - l) / 2;
+		if (mid * mid * mid >= x) {
+			r = mid;
+		}
+		else {
+			l = mid;
+		}
+	}
+
+	cout << setprecision(20) << l << endl;
+}
