60109d8caec68253ff76d404fad658955947ac33
Plamen Atanasov <plamen.atanasov25@gmail.com>
2024-05-13 15:05:17 +0300

    Add solutions

---
+++ Solutions/CountComponents.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+namespace {
+	constexpr int MAX = 1'000'000;
+	int n, m;
+	int s[MAX], arr[MAX];
+}
+
+int find(int x) {
+	if (arr[x] == -1) {
+		return x;
+	}
+
+	return find(arr[x]);
+}
+
+void myUnion(int x, int y, int& ccount) {
+	int rX = find(x);
+	int rY = find(y);
+	if (rX == rY) return;
+
+	ccount--;
+
+	if (s[rX] <= s[rY]) {
+		arr[rX] = rY;
+		s[rX] += 1;
+	}
+	else {
+		arr[rY] = rX;
+		s[rY] += 1;
+	}
+}
+
+void countComponents() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	fill(arr, arr + MAX, -1);
+
+	cin >> n >> m;
+	int x, y;
+	int ccount = n;
+	for (int i = 1; i <= m; i++) {
+		cin >> x >> y;
+		myUnion(x, y, ccount);
+	}
+
+	cout << ccount << endl;
+}
---
+++ Solutions/Viruses.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+namespace {
+	constexpr int MAX = 10'000;
+	int n, m, k;
+	vector<int> neighbours[MAX];
+	bool visited[100][100];
+	int ccount;
+}
+
+void DFS(int x, int y) {
+	visited[x][y] = true;
+	for (int z : neighbours[x * m + y]) {
+		if (!visited[z / m][z % m]) {
+			DFS(z / m, z % m);
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	cin >> n >> m >> k;
+	int x, y;
+	for (int i = 1; i <= k; i++) {
+		cin >> x >> y;
+		visited[x - 1][y - 1] = true;
+	}
+
+	for (int i = 0; i < n * m; i++) {
+		int row = i / m;
+		int col = i % m;
+		if (row + 1 < n) {
+			neighbours[i].push_back(i + m);
+		}
+		if (row - 1 >= 0) {
+			neighbours[i].push_back(i - m);
+		}
+		if (col + 1 < m) {
+			neighbours[i].push_back(i + 1);
+		}
+		if (col - 1 >= 0) {
+			neighbours[i].push_back(i - 1);
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (!visited[i][j]) {
+				ccount++;
+				DFS(i, j);
+			}
+		}
+	}
+
+	cout << ccount << endl;
+}
